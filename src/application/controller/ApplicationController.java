07992b160011d08cdf6d1208fbb1b166b64e0bfd
Muhammad Islam <Muhammad.R.Islam@stonybrook.edu>
2019-05-04 02:10:46 -0400

    Implemented skeleton controllers

--- src/application/controller/ApplicationController.java
+++ src/application/controller/ApplicationController.java
@@ -9,13 +9,21 @@ import java.util.Random;
 
 public class ApplicationController {
 
-    @FXML
-    public Label labelMsg;
-
     public void generateRandom(ActionEvent actionEvent) {
         Random rand = new Random();
         int myRand = rand.nextInt(50) + 1;
-        labelMsg.setText(String.valueOf(myRand));
         System.out.println(myRand);
+    }
+
+    public void handleSignInButtonAction(ActionEvent actionEvent) {
+        System.out.println("Sign in Button Pressed");
+    }
+
+    public void handleRegisterButtonAction(ActionEvent actionEvent) {
+        System.out.println("Register button pressed");
+    }
+
+    public void handleSignOutButtonAction(ActionEvent actionEvent) {
+        System.out.println("Sign Out Button Pressed");
     }
 }
