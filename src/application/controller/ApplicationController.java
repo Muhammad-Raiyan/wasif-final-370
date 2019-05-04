d51db516bfe69ff6c1c9ee98d59e7d707c3f9426
Muhammad Islam <Muhammad.R.Islam@stonybrook.edu>
2019-05-04 03:36:01 -0400

    Signin signout button strategy implemented

--- src/application/controller/ApplicationController.java
+++ src/application/controller/ApplicationController.java
@@ -1,22 +1,42 @@
 package application.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.event.ActionEvent;
-import javafx.scene.control.Label;
 
-import java.util.Random;
 
 public class ApplicationController {
 
-    public void generateRandom(ActionEvent actionEvent) {
-        Random rand = new Random();
-        int myRand = rand.nextInt(50) + 1;
-        System.out.println(myRand);
+    @FXML
+    public Button LSB_signOutButton;
+    @FXML
+    public Button LSB_registerButton;
+    @FXML
+    public Button LSB_signInInButton;
+
+    private static  boolean isUserSignedIn;
+
+    public ApplicationController() {
+        System.out.println("Constructor Called");
+        isUserSignedIn = false;
+    }
+
+    @FXML
+    public void initialize(){
+        System.out.println("Initialized called");
+        LSB_signOutButton.setDisable(true);
     }
 
     public void handleSignInButtonAction(ActionEvent actionEvent) {
         System.out.println("Sign in Button Pressed");
+        if(!isUserSignedIn){
+            isUserSignedIn = true;
+            LSB_signOutButton.setDisable(false);
+            LSB_signInInButton.setText("User");
+            System.out.println("Signed in user");
+        } else {
+            System.out.println("Profile Button Pressed");
+        }
     }
 
     public void handleRegisterButtonAction(ActionEvent actionEvent) {
@@ -25,5 +45,11 @@ public class ApplicationController {
 
     public void handleSignOutButtonAction(ActionEvent actionEvent) {
         System.out.println("Sign Out Button Pressed");
+        if(isUserSignedIn){
+            isUserSignedIn = false;
+            LSB_signOutButton.setDisable(true);
+            LSB_signInInButton.setText("Sign In");
+            System.out.println("Signing out user");
+        }
     }
 }
